6635fcd53c53959b8b90efc9e8f1f5f272226ba2
sandunvirajdilshan <sandunvirajdilshan.s@gmail.com>
2023-08-20 21:20:14 +0530

    04

--- python.py
+++ python.py
@@ -1,15 +1,2 @@
-nums = [1, 3, 5, 6]
-number = int(input("Enter a Number: "))
-s = 0
-e = len(nums) - 1
-while s <= e:
-    m = (s + e) // 2
-    if nums[m] == number:
-        print(m)
-        break
-    elif nums[m] < number:
-        s = m + 1
-    else:
-        e = m - 1
-else:
-    print("-1")
+s = input("Enter Something: ")
+print(len(s.split()[-1]))
