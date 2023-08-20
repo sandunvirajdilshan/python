f98ba2bb5df4f4f3d6030b7c9c237cd02047d98f
sandunvirajdilshan <sandunvirajdilshan.s@gmail.com>
2023-08-20 19:57:44 +0530

    02

--- python.py
+++ python.py
@@ -1,10 +1,6 @@
-list_1 = [1,10,7,6]
-list_2 = [4,7,2]
-list_1.extend(list_2)
-list_1.sort()
-length = len(list_1)
-if (length%2 == 0):
-    median = (list_1[length//2]+list_1[(length//2)-1])/2
+x = input("Enter a Number:- ")
+y = x[::-1]
+if x == y:
+    print(True)
 else:
-    median = list_1[length//2]
-print(median)
+    print(False)
